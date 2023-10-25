b5a3c6c05fbab7673f6f743b5aa7869839917341
marras0914 <arras.marco@gmail.com>
2023-10-25 11:15:20 -0500

    adding titanshield png for menu collapse

--- src/components/Icons/SidebarMenus/MenuCollapsedIcon.tsx
+++ src/components/Icons/SidebarMenus/MenuCollapsedIcon.tsx
@@ -16,49 +16,15 @@
 
 import * as React from "react";
 import { SVGProps } from "react";
+const titanShield = require("../../../components/assets/titanShield.png"); // "./titanShield.png";
 
 const MenuCollapsedIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className={`min-icon`}
-    fill={"currentcolor"}
-    viewBox="0 0 15 15"
-    {...props}
-  >
-    <g id="Grupo_2449" data-name="Grupo 2449" transform="translate(-140 -181)">
-      <rect
-        id="Rectángulo_1589"
-        data-name="Rectángulo 1589"
-        width="15"
-        height="15"
-        rx="2"
-        transform="translate(140 181)"
-        fill="#08193a"
-        opacity="0.601"
-      />
-      <g id="OpenListIcon-full" transform="translate(144 250.612)">
-        <g
-          id="noun_chevron_2320228"
-          transform="translate(6.827 -63.612) rotate(90)"
-        >
-          <path
-            id="Trazado_6842"
-            data-name="Trazado 6842"
-            d="M.422,6.661a.433.433,0,0,1-.3-.117.37.37,0,0,1,0-.557L2.983,3.335.126.675a.37.37,0,0,1,0-.557.443.443,0,0,1,.6,0L3.889,3.052a.373.373,0,0,1,.126.274.344.344,0,0,1-.126.274L.727,6.533a.443.443,0,0,1-.306.127Z"
-            transform="translate(0 0)"
-          />
-        </g>
-        <rect
-          id="Rectángulo_896"
-          data-name="Rectángulo 896"
-          width="0.462"
-          height="0.462"
-          transform="translate(0 -61.808)"
-          fill="none"
-        />
-      </g>
-    </g>
-  </svg>
+  <img
+    src={titanShield}
+    alt="Titan Cloud Storage Logo"
+    width={30}
+    height={30}
+  />
 );
 
 export default MenuCollapsedIcon;
